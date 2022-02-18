146867173553a42723363de3406103d0b04b46e9
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2022-02-18 23:39:33 +0600

    Add files via upload

---
+++ Basic MJ/getdata.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn= mysqli_connect("localhost","root","");
+mysqli_select_db($conn,"android_db");
+
+
+$qry="SELECT * FROM tbl_staff";
+
+//query fetch
+$raw=mysqli_query($conn,$qry);
+
+//store all data in the array by while loop 
+while($res=mysqli_fetch_array($raw))
+{
+    $data[]=$res;
+}
+
+print(json_encode($data));
+
+
+?>
+
+
+//10.0.2.2
